71af47c77541bd7d8a7eacfc920bfff637e4a8e8
wangDuDu <496977440@qq.com>
2022-06-09 23:43:29 +0800

    feat:找出两个链表的交点

---
+++ src/linkedlist/ListNode.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName ListNode.java
+ * @Description TODO
+ * @createTime 2022年06月09日 22:32:00
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
