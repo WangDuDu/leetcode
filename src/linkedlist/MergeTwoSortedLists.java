a7059e9e7b8bb53b7d48bdad9d022c3c24dd25b0
wangDuDu <496977440@qq.com>
2022-06-14 00:16:27 +0800

    feat:合并链表两种解法

---
+++ src/linkedlist/MergeTwoSortedLists.java
@@ -0,0 +1,109 @@
+package linkedlist;
+
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [], l2 = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两个链表的节点数目范围是 [0, 50] 
+// -100 <= Node.val <= 100 
+// l1 和 l2 均按 非递减顺序 排列 
+// 
+// Related Topics 递归 链表 👍 2471 👎 0
+
+
+import leetcode.editor.cn.ListNode;
+
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        Solution solution = new MergeTwoSortedLists().new Solution();
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        /**
+         * 递归解法
+         *
+         * @param list1
+         * @param list2
+         * @return
+         */
+        public leetcode.editor.cn.ListNode mergeTwoLists(leetcode.editor.cn.ListNode list1, leetcode.editor.cn.ListNode list2) {
+            if (list1 == null) {
+                return list2;
+            }
+            if (list2 == null) {
+                return list1;
+            }
+            if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+
+        /**
+         * 循环解法
+         *
+         * @param list1
+         * @param list2
+         * @return
+         */
+        public leetcode.editor.cn.ListNode mergeTwoLists2(leetcode.editor.cn.ListNode list1, leetcode.editor.cn.ListNode list2) {
+            // preHead和prev都指向了同一个ListNode对象，val = 1, next = null
+            leetcode.editor.cn.ListNode preHead = new leetcode.editor.cn.ListNode(-1);
+            ListNode prev = preHead;
+            while (list1 != null && list2 != null) {
+                // 在第一次循环的时候prev和preHead的next都指向了合并后链表的头节点，也就是两个链表的头结点中比较小的那一个
+                if (list1.val < list2.val) {
+                    prev.next = list1;
+                    list1 = list1.next;
+                } else {
+                    prev.next = list2;
+                    list2 = list2.next;
+                }
+                // prev在这里指向了比较小的节点，preHead还是指向-1的节点
+                prev = prev.next;
+            }
+            prev.next = list1 == null ? list2 : list1;
+            return preHead.next;
+        }
+    }
+
+}
